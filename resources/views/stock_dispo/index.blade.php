c2274d538c819002c2533ecdbf1e62c2a86e0238
chihebttss <chihebbouabidi@gmail.com>
2023-03-23 15:36:19 +0100

    first commit

---
+++ resources/views/stock_dispo/index.blade.php
@@ -0,0 +1,92 @@
+@extends('layout.main') @section('content')
+
+    @if($errors->has('name'))
+        <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ $errors->first('name') }}</div>
+    @endif
+    @if($errors->has('image'))
+        <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ $errors->first('image') }}</div>
+    @endif
+    @if(session()->has('message'))
+        <div class="alert alert-success alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('message') }}</div>
+    @endif
+    @if(session()->has('not_permitted'))
+        <div class="alert alert-danger alert-dismissible text-center"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>{{ session()->get('not_permitted') }}</div>
+    @endif
+
+    <section>
+        <div class="table-responsive">
+            <table id="stock-table" class="table" style="width: 100%">
+                <thead>
+                <tr>
+                    <th class="not-exported"></th>
+                    <th>id</th>
+                    <th>{{trans('file.warehouse')}}</th>
+                    <th>{{trans('file.Product')}}</th>
+                    <th>{{trans('file.Stock Quantity')}}</th>
+                </tr>
+                </thead>
+            </table>
+        </div>
+    </section>
+
+
+@endsection
+@push('scripts')
+    <script type="text/javascript">
+
+
+        $('#stock-table').DataTable( {
+            "processing": true,
+            "serverSide": true,
+            "ajax":{
+                url:"stock/stock-data",
+                dataType: "json",
+                type:"post"
+            },
+            "createdRow": function( row, data, dataIndex ) {
+                $(row).attr('data-id', data['id']);
+            },
+            "columns": [
+                {"data": "key"},
+                {"data": "entrepot"},
+                {"data": "produit"},
+                {"data": "stock_qty"},
+            ],
+            'language': {
+                'lengthMenu': '_MENU_ {{trans("file.records per page")}}',
+                "info":      '<small>{{trans("file.Showing")}} _START_ - _END_ (_TOTAL_)</small>',
+                "search":  '{{trans("file.Search")}}',
+                'paginate': {
+                    'previous': '<i class="dripicons-chevron-left"></i>',
+                    'next': '<i class="dripicons-chevron-right"></i>'
+                }
+            },
+            order:[['2', 'asc']],
+            'columnDefs': [
+                {
+                    "orderable": false,
+                    'targets': [0, 1, 3, 4, 5, 6, 7]
+                },
+                {
+                    'render': function(data, type, row, meta){
+                        if(type === 'display'){
+                            data = '<div class="checkbox"><input type="checkbox" class="dt-checkboxes"><label></label></div>';
+                        }
+
+                        return data;
+                    },
+                    'checkboxes': {
+                        'selectRow': true,
+                        'selectAllRender': '<div class="checkbox"><input type="checkbox" class="dt-checkboxes"><label></label></div>'
+                    },
+                    'targets': [0]
+                }
+            ],
+            'select': { style: 'multi',  selector: 'td:first-child'},
+            'lengthMenu': [[10, 25, 50, -1], [10, 25, 50, "All"]],
+
+            dom: '<"row"lfB>rtip',
+        } );
+
+    </script>
+@endpush
